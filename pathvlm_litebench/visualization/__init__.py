ff8d54f9bbbf5ebb8200957d7a4b5aa764a638b8
GuoYixuan0130 <15332075537@163.com>
2026-04-27 11:36:49 +0800

    Add HTML retrieval report utility

--- pathvlm_litebench/visualization/__init__.py
+++ pathvlm_litebench/visualization/__init__.py
@@ -1,3 +1,7 @@
 from .topk_viewer import save_topk_image_grids
+from .html_report import save_retrieval_html_report
 
-__all__ = ["save_topk_image_grids"]
+__all__ = [
+    "save_topk_image_grids",
+    "save_retrieval_html_report",
+]
